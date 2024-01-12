ca8d709b66f878d6116585d4e3711e7c7eaeed7c
BbqGamer <korba.adam@gmail.com>
2024-01-12 17:03:46 +0100

    Add simple express js endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import OpenAI from 'openai';
+import Express from 'express';
+
+var app = Express();
+
+app.get('/query/:message', async (req, res) => {
+    var response = await queryopenai(req.params.message);
+    res.end(JSON.stringify({
+        response: response
+    }))
+});
 
 const openai = new OpenAI();
 
-async function main() {
+async function queryopenai(query: string): Promise<string | null> {
+    console.log('Querying OpenAI');
     const chatCompletion = await openai.chat.completions.create({
         messages: [{ role: 'user', content: 'Say this is a test' }],
         model: 'gpt-3.5-turbo',
     });
-    console.log(chatCompletion.choices[0].message.content);
+    return chatCompletion.choices[0].message.content;
 }
 
-main();
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+})
+
