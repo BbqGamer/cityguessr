039f622ea261862384516b938c2f3cf39927b07c
BbqGamer <korba.adam@gmail.com>
2024-01-13 11:24:08 +0100

    Create base index page and asset build scripts

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,20 @@ import Express from 'express';
 import { ChromaClient } from 'chromadb';
 import { getCollection, createCollection } from './chroma/collection';
 import { cityRouter } from './controllers/CityController';
+import path from 'path';
 
 
 function main() {
     const app = Express();
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+    app.use(Express.static(path.join(__dirname, 'public')));
+
     const client = new ChromaClient();
+
+    app.get('/', (req, res) => {
+        res.render('index');
+    });
 
     app.get('/collection/get/:name', async (req, res) => {
         res.end(await getCollection(client, req.params.name));
