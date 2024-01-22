a417afcae8451f6652e1f9df69c8a7f121e9f0f1
BbqGamer <korba.adam@gmail.com>
2024-01-22 14:34:55 +0100

    Polish the game queue

--- src/services/game.ts
+++ src/services/game.ts
@@ -1,7 +1,13 @@
 import { Server, Socket } from 'socket.io'
 import { SessionUser } from '../models/User';
 
-let usersInQueue: SessionUser[] = [];
+interface QueueUser extends SessionUser {
+    socket: Socket;
+    ready: boolean;
+}
+
+let usersInQueue: QueueUser[] = [];
+let gameStarted = false;
 
 export function handleConnection(io: Server, socket: Socket) {
     const req: any = socket.request
@@ -20,14 +26,51 @@ export function handleConnection(io: Server, socket: Socket) {
             socket.emit('drop', 'You are already in the queue')
             return socket.disconnect();
         }
+        const user: QueueUser = {
+            ...session.user,
+            socket: socket,
+            ready: false,
+        }
         usersInQueue.push(session.user);
 
         io.emit('queue', usersInQueue);
     }
+
+    socket.on('ready', () => {
+        const user = usersInQueue.find(u => u.user_id === session.user.user_id);
+        if (gameStarted) {
+            socket.emit('drop', 'Game already started');
+            return socket.disconnect();
+        }
+        if (user) {
+            user.ready = true;
+            console.log(usersInQueue)
+            io.emit('queue', usersInQueue);
+        }
+        if (usersInQueue.length >= 2 && usersInQueue.every(u => u.ready)) {
+            gameStarted = true;
+            io.emit('game-start', usersInQueue);
+        }
+    });
+
+    socket.on('unready', () => {
+        if (gameStarted) {
+            socket.emit('drop', 'Game already started');
+            return socket.disconnect();
+        }
+        const user = usersInQueue.find(u => u.user_id === session.user.user_id);
+        if (user) {
+            user.ready = false;
+            console.log(usersInQueue)
+            io.emit('queue', usersInQueue);
+        }
+    });
 
     socket.on('disconnect', () => {
         console.log('User disconnected user: ', session.user.username);
         usersInQueue = usersInQueue.filter(u => u.user_id !== session.user.user_id);
         io.emit('queue', usersInQueue);
     });
+
+
 }
